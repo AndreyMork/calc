e0519d5b007c8597e010565eb1a1caab5fc3873d
Andrey <magot.a.d@gmail.com>
2018-10-21 16:15:55 +0300

    add bin file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import tokenize from './tokenize';
 
-const dataString = '123 +/-*abc ab*12c 12.5 .2  123';
-const tokens = tokenize(dataString);
-console.log(tokens);
+export default (inputStr) => {
+  const tokens = tokenize(inputStr);
+
+  return tokens;
+};
