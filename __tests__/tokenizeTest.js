424b06bccf3822a72ac6610774b277fa0fa668eb
Andrey <magot.a.d@gmail.com>
2018-10-22 19:50:52 +0300

    update tokenize tests

--- __tests__/tokenizeTest.js
+++ __tests__/tokenizeTest.js
@@ -3,11 +3,37 @@ import tokenize from '../src/tokenize';
 describe('tokenization', () => {
   test('tokenization', () => {
     const inputStr = 'abc + 123 && ';
-    const expectedTokens = ['abc', '+', '123', '&', '&'];
+    const expectedTokens = [
+      { val: 'abc', type: 'id' },
+      { val: '+', type: 'operator' },
+      { val: '123', type: 'num' },
+      { val: '&&', type: 'trash' },
+    ];
     expect(tokenize(inputStr)).toEqual(expectedTokens);
 
-    const inputStr2 = ' abc + __a_b12-.5* 432   \t   / 12.2';
-    const expectedTokens2 = ['abc', '+', '__a_b12', '-', '.5', '*', '432', '/', '12.2'];
+    const inputStr2 = ' + __a_b12-.5* 432 2.   \t .  a..a 12.2';
+    const expectedTokens2 = [
+      { val: '+', type: 'operator' },
+      { val: '__a_b12', type: 'id' },
+      { val: '-', type: 'operator' },
+      { val: '.5', type: 'num' },
+      { val: '*', type: 'operator' },
+      { val: '432', type: 'num' },
+      { val: '2.', type: 'num' },
+      { val: '.', type: 'trash' },
+      { val: 'a..a', type: 'trash' },
+      { val: '12.2', type: 'num' },
+    ];
+
     expect(tokenize(inputStr2)).toEqual(expectedTokens2);
+
+    const trashStr = '2abc .abc  12.abc 12.3&&';
+    const expectedTokens3 = [
+      { val: '2abc', type: 'trash' },
+      { val: '.abc', type: 'trash' },
+      { val: '12.abc', type: 'trash' },
+      { val: '12.3&&', type: 'trash' },
+    ];
+    expect(tokenize(trashStr)).toEqual(expectedTokens3);
   });
 });
