b237fac96b581591422dc33b21f6a65ffb645717
Andrey <magot.a.d@gmail.com>
2018-10-24 20:27:10 +0300

    chore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import tokenize from './tokenize';
 import { mainLog } from './loggers';
 
-
+// TODO token string validation
 export default (inputStr) => {
   mainLog(`input is '${inputStr}'`);
   const tokens = tokenize(inputStr);
