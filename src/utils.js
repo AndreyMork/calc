5ebd71a2b4ea70a074a380bef8552b8f1d1659db
Andrey <magot.a.d@gmail.com>
2018-10-24 11:30:12 +0300

    add logging to isNumber

--- src/utils.js
+++ src/utils.js
@@ -7,10 +7,10 @@ const utilsLog = logger.extend('utils');
 export const getTypeOfChar = (char) => {
   const log = utilsLog.extend('getTypeOfChar');
   if (!_.isString(char)) {
-    log(char);
+    log(`${char} is not a string`);
     throw new Error(`${char} is not a string.`);
   } else if (char.length !== 1) {
-    log(char);
+    log(`'${char}' is not a single character.`);
     throw new Error(`'${char}' is not a single character.`);
   }
 
@@ -29,16 +29,23 @@ export const getTypeOfChar = (char) => {
 
 
 export const isNumber = (value) => {
+  const log = utilsLog.extend('isNumber');
+
   const typeOfValue = typeof value?.valueOf?.();
   if (typeOfValue !== 'string' && typeOfValue !== 'number') {
+    log(`${value} is not a number`);
     return false;
   }
   if (value === '') {
+    log(`${value} is not a number`);
     return false;
   }
 
   const castedToNum = Number(value);
-  return !Number.isNaN(castedToNum) && Number.isFinite(castedToNum);
+  const res = !Number.isNaN(castedToNum) && Number.isFinite(castedToNum);
+  log(`${value} is ${res ? '' : 'not '}a number`);
+
+  return res;
 };
 
 
