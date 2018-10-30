958894ae0dc615444b853f7a8168707631ad0b88
Andrey <magot.a.d@gmail.com>
2018-10-30 20:00:44 +0300

    add more tests

--- __tests__/appTest.js
+++ __tests__/appTest.js
@@ -4,5 +4,12 @@ import calc from '../src';
 describe('app test', () => {
   it('calc', () => {
     expect(calc('2 * 5 + 2^(2 - 2) / 0.5')).toBe(12);
+    expect(calc('-2')).toBe(-2);
+    expect(calc('--2')).toBe(2);
+    expect(calc('+2')).toBe(2);
+    expect(calc('1--1')).toBe(2);
+    expect(calc('2^-1')).toBe(0.5);
+    expect(calc('2^(-1)^2')).toBe(2);
+    expect(calc('2^-1^2')).toBe(0.5);
   });
 });
